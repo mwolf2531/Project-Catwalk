23aae3a2ba4f36ae248ab41e74f0435be439a95c
Ryder Wendt <ryderw93@gmail.com>
2021-12-27 15:34:28 -0500

    added component files and basic css grid template for overview widget

---
+++ src/components/Overview/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ImageGallery extends Component {
+  render() {
+    return (
+      <div>
+      Image Gallery
+      </div>
+    )
+  }
+}
+
+export default ImageGallery

7107b51a6a53ee7dc54c76911162640d83a35814
Alex Kenney <dareitus@gmail.com>
2021-12-30 16:26:06 -0500

    completed routes

--- server/routes.js
+++ server/routes.js
@@ -19,10 +19,28 @@ router.get('/reviews', (req, getRes) => {
     url: url + '/reviews',
     headers: headers,
     params: {
-      page: 1,
-      count: 5,
-      sort: 'relevant',
-      product_id: 37314
+      page: 1, //review page to pull from
+      count: 5, //number of reviews to pull
+      sort: 'relevant', //placeholder - replace with currently selected sort method
+      product_id: 37314 //placeholder - replace with id of current product
+    }
+  };
+  axios(options)
+    .then(function (res) {
+      console.log(res.data);
+      getRes.send(res.data)
+    })
+    .catch(function (err) { getRes.send(err) });
+})
+// get "metaData" - pulls all review meta data for the current item
+router.get('/revMeta', (req, getRes) => {
+  //Call API, Get review data
+  let options = {
+    method: 'GET',
+    url: url + '/reviews/meta',
+    headers: headers,
+    params: {
+      product_id: 37314 //placeholder - replace with id of current product
     }
   };
   axios(options)
@@ -33,8 +51,68 @@ router.get('/reviews', (req, getRes) => {
     .catch(function (err) { getRes.send(err) });
 })
 // post "review" - posts a new user created review
-// post "helpful" - posts a user toggle of the "helpful" trait review
-// post "report" - posts a user toggle of a particular report option (bad language, hate speech, etc) - Can be faked
+router.post('/reviews', (req, postRes) => {
+  //Call API, Get review data
+  let options = {
+    method: 'POST',
+    url: url + '/reviews',
+    headers: headers,
+    params: {
+      product_id: 37314, //placeholder - replace with id of current product
+      rating: 5, //placeholder - takes in 1-5 for star rating
+      summary: 'User Entered Title Here', //placeholder
+      body: 'User Entered Review Body Goes Here', //placeholder
+      recommend: true, //placeholder - boolean representing if user recommends product
+      name: 'User Entered Username', //placeholder
+      email: 'User@Entered.Email', //placeholder
+      photos: [], //placeholder - array of text urls that link to user uploaded images
+      characteristics: {} //placeholder - object of keys representing characteristic_id and values representing review value
+      //example, "size" is characteristic 14. {14:3} is perfect size. {14:5} is a full size too big {14:2} is a half size small
+    }
+  };
+  axios(options)
+    .then(function (res) {
+      console.log(res.data);
+      postRes.send(res.data)
+    })
+    .catch(function (err) { postRes.send(err) });
+})
+// put "helpful" - posts a user toggle of the "helpful" trait review
+router.put('/helpful', (req, putRes) => {
+  //Call API, Get review data
+  let options = {
+    method: 'PUT',
+    url: url + '/reviews/1074988/helpful', //replace 1074988 with current review id
+    headers: headers,
+    params: {
+      review_id: 1074988 //placeholder - replace with id of current review
+    }
+  };
+  axios(options)
+    .then(function (res) {
+      console.log(res.data);
+      putRes.send(res.data)
+    })
+    .catch(function (err) { putRes.send(err) });
+})
+// put "report" - posts a user toggle of a particular report option (bad language, hate speech, etc)
+router.put('/report', (req, putRes) => {
+  //Call API, Get review data
+  let options = {
+    method: 'PUT',
+    url: url + '/reviews/1074988/report', //replace 1074988 with current review id
+    headers: headers,
+    params: {
+      review_id: 1074988 //placeholder - replace with id of current review
+    }
+  };
+  axios(options)
+    .then(function (res) {
+      console.log(res.data);
+      putRes.send(res.data)
+    })
+    .catch(function (err) { putRes.send(err) });
+})
 //MEGAN'S ROUTES
 
 //RYDER's ROUTES

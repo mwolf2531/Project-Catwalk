209ae186dc0500260d0ea25b4fd94713099af482
Megan Wolf <meganwolf25@gmail.com>
2021-12-30 12:54:07 -0500

    API Routes Established

--- server/routes.js
+++ server/routes.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
+const axios = require('axios');
+//Server info variable (API)
+const url = `https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rfe`;
+//Header (API Auth)
+const headers = require('./APIHelper.js');
 //SUPPORT ROUTES
 //test route at /api/test
 router.get('/test', (req, res) => {
   res.send('API Router says Hello World!');
 })
-
 //ALEX's ROUTES
 // get "allReviews" - pulls all review data for the current item
 router.get('/reviews', (req, res) => {
-  res.send('API REVIEW CALL');
+  //Call API, Get review data
+  let options = {
+    method: 'GET',
+    url: url + '/reviews',
+    headers: headers
+  };
+  axios(options)
+    .then(function (res) { res.send(null, res) })
+    .catch(function (err) { res.send(err) });
 })
 // post "review" - posts a new user created review
 // post "helpful" - posts a user toggle of the "helpful" trait review
 // post "report" - posts a user toggle of a particular report option (bad language, hate speech, etc) - Can be faked
-
 //MEGAN'S ROUTES
 
 //RYDER's ROUTES
-
 module.exports = router;

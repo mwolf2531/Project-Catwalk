1c3d4798a6d5cb7bb631b11f5f072d3b1980621c
Ryder Wendt <ryderw93@gmail.com>
2022-01-01 14:54:46 -0500

    cleaned up a lot of commented code out

--- src/components/Overview/StyleSelector.jsx
+++ src/components/Overview/StyleSelector.jsx
@@ -21,43 +21,7 @@ const StyleSelector = ({ styles, loaded, onClick }) => {
   }
 };
 
-// const StyleSelector = ({ styles, loaded }) => {
-//   console.log("styles!!!!:", styles);
-//   console.log("loaded:", loaded);
-//   return (
-//     <div className="style-thumbnail-container">
-//       {styles.map((item, i) => (
-//         <Styles key={i} style={item.name} photos={item.photos} />
-//       ))}
-//     </div>
-//   );
-// };
+
 
 export default StyleSelector;
-// import React, { Component } from "react";
-// import Styles from "./Styles.jsx";
 
-// export class StyleSelector extends Component {
-//   constructor(props) {
-//     super(props);
-//     // TODO: add state
-//     // array of all styles for product
-//     // TODO: don't forget to bind functions and stuff
-//   }
-
-//   // TODO: api call to GET /products/:product_id/styles to get all styles
-//   // for product and set state to that list
-
-//   render() {
-//     return (
-//       <div>
-//         {styles.results.map((item, i) => {
-//           <Styles key={item.style_id} photos={item.photos} />;
-//         })}
-//       </div>
-//     );
-//   }
-// }
-// // TODO: Pass array of styles to Styles component
-
-// export default StyleSelector;
--- src/components/Overview/Styles.jsx
+++ src/components/Overview/Styles.jsx
@@ -3,15 +3,9 @@ import React from "react";
 const Styles = ({ style, photos, onClick, item }) => {
   return (
     <>
-      {/* {style} */}
-
-        {/* {photos.map((item, i) => {
-          return <img key={i} src={item.thumbnail_url} />;
-        })} */}
         <img className='style-thumbnail'
         onClick={() => onClick(item)}
          src={photos[0].thumbnail_url} />
-
     </>
   );
 };

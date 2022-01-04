827f7421471c031577c0b387bb3abdd23402b3b2
Ryder Wendt <ryderw93@gmail.com>
2022-01-04 17:08:33 -0500

    fixed full screen image and worked on cart

--- src/components/Overview/AddToCart.jsx
+++ src/components/Overview/AddToCart.jsx
@@ -6,7 +6,9 @@ export class AddToCart extends Component {
     super(props);
     this.state = {
       default: {},
+      clickedStyle: {},
     };
+    this.handleSize = this.handleSize.bind(this)
   }
 
   componentDidUpdate(prevProps) {
@@ -17,6 +19,12 @@ export class AddToCart extends Component {
     }
   }
 
+  handleSize(event) {
+    // this.setState({
+    //   quantity: event
+    // })
+  }
+
   render() {
     const buttonStyle = {
       color: "grey",
@@ -25,33 +33,48 @@ export class AddToCart extends Component {
       textalign: "end",
     };
 
-    console.log(this.state.default);
-
     let list = Object.values(this.state.default);
 
-    // const options = list.map((sku, index) => {
-    //   return {
-    //     label: sku.size,
-    //     value: sku.size,
-    //     key: index,
-    //   };
-    // });
-    // console.log(options);
+    let dummyData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    let testing = (
+      <select className="select-size" onChange={() => this.handleSize()}>
+        <option>SELECT SIZE</option>
+        {list.map((sku, index) => (
+          <option key={index} value={sku.size}>
+            {sku.size}
+          </option>
+        ))}
+      </select>
+    );
+
+    if (this.props.item !== undefined) {
+      let currentList = Object.values(this.props.item);
+      let testing2 = (
+        <select className="select-size">
+          <option>SELECT SIZE</option>
+          {currentList.map((sku, index) => (
+            <option key={index} value={sku.size}>
+              Size: {sku.size}
+            </option>
+          ))}
+        </select>
+      );
+      testing = testing2;
+    }
 
     return (
       <div className="cart-container">
 
-        <select className="select-size" >
-        <option>SELECT SIZE</option>
-        {list.map((sku, index) => (
-            <option key={index} value={sku.size}>
-              {sku.size}
+        {testing}
+
+        <select className="select-quantity">
+          <option>-</option>
+          {dummyData.map((sku, index) => (
+            <option key={index} value={sku}>
+              {sku}
             </option>
           ))}
-        </select>
-
-        <select name='name' className="select-quantity" value="-">
-          <option>-</option>
         </select>
 
         <div>
@@ -66,3 +89,5 @@ export class AddToCart extends Component {
 }
 
 export default AddToCart;
+
+
